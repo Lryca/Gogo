931e97f5f0d5c5f37c6724ba1249483ae95150e7
Lryca <72592692+Lryca@users.noreply.github.com>
2021-07-09 16:34:03 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix = '-')
+
+@client.event
+async def on_ready():
+
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.online)
+
+  await client.change_presence(activity=discord.Game(name="노래"))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+  
+  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+
+
+client.run(os.environ['ODAxNjk0ODc5MDM3MjU5Nzgw.YAkaiQ.gF_rECUwEj7j36aKjwan-Q_9vYE'])
